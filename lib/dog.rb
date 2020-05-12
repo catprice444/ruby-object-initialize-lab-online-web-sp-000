7cbd9f9b288c09b97dcc53d0e1275afe6dce00f6
Catherine Price <catprice444@gmail.com>
2020-05-12 22:14:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog
-attr_accessor :name, :breed
+attr_accessor :name
 
   def initialize(name, breed)
     @name = name
     @breed = breed
-    if breed == nil
-      puts "Mutt"
-    end
   end
-
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end 
 end
